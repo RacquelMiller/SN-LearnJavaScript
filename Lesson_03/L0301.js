029e686994acff03bd28de313841bd414368813c
Racquel <racquelsteph@gmail.com>
2025-05-08 16:30:42 -0400

    Update L0301.js

--- Lesson_03/L0301.js
+++ Lesson_03/L0301.js
@@ -1,12 +1,12 @@
 //
 // L03S01 - Simple variables & good/bad variable names
 //
-var name   = 'Chuck';   // Simple string variable
-var i      = 0;         // Simple integer variable
+var name   = 'Racquel';   // Simple string variable
+var i      = 5;         // Simple integer variable
 var answer = true;      // Simple boolean variable
 
 // Naming
-var c = "http://www.amazon.com";  // Not ideal
+var n = "http://www.nike.com";  // Not ideal
 var case = 'CASE0010001';         // Reserved word!
 var lastEntryInTheListWithRelatedRecords = true; // a bit long?
 // personCount      // Indicates a counter/integer

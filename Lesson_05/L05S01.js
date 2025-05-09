572580e014975e6a433a06f7ab9b02f60cebb11c
Racquel <racquelsteph@gmail.com>
2025-05-08 21:19:58 -0400

    Update L05S01.js

--- Lesson_05/L05S01.js
+++ Lesson_05/L05S01.js
@@ -1,5 +1,11 @@
 //
 // L05S01 - Common error messages
 //
+gs.info(myUnknownVariable); // myUnknownVariable variable is not declared
+ga.info('Great day we are having!); // ga.info variable is incorrect and second quote is not present
+//
+// Correction
+var myUnknownVariable = 'Hello, sunshine!';
+
 gs.info(myUnknownVariable);
-ga.info('Hello, world!);
+gs.info('Great day we are having!');

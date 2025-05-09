0b6ad1ac20040fd623ad6eb70b70ae8410456045
Racquel <racquelsteph@gmail.com>
2025-05-08 20:03:11 -0400

    Update L0301.js

--- Lesson_03/L0301.js
+++ Lesson_03/L0301.js
@@ -1,15 +1,15 @@
 //
 // L03S01 - Simple variables & good/bad variable names
 //
-var name   = 'Racquel';   // Simple string variable
+var name   = 'Racquel';   // Simple string variable, single quotes or double quotes- be consistent
 var i      = 5;         // Simple integer variable
-var answer = true;      // Simple boolean variable
+var answer = true;      // Simple boolean variable, boolean recognized as a JavaScript keyword, it is a different color
 
 // Naming
-var n = "http://www.nike.com";  // Not ideal
+var n = "http://www.nike.com";  // Not ideal, does not specify the URL name 
 var case = 'CASE0010001';         // Reserved word!
-var lastEntryInTheListWithRelatedRecords = true; // a bit long?
+var lastEntryInTheListWithRelatedRecords = true; // too long
 // personCount      // Indicates a counter/integer
 // personList       // Must be a list
 // personObj        // Object
-// personGr         // GlideRecord
+// personGr         // GlideRecord, record from database

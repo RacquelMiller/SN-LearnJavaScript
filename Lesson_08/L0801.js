ed3fc475939054368f71eae7e77a608f0b0c187a
Racquel <racquelsteph@gmail.com>
2025-05-08 22:41:16 -0400

    Update L0801.js

--- Lesson_08/L0801.js
+++ Lesson_08/L0801.js
@@ -2,9 +2,9 @@
 //  L08S01 - Changing and detecting variable types
 //
 var i = 21;
-var iStr = i.toString(); // toString converts integer to a string, just in case you want the string part
-gs.info(typeof i); // 
+var iStr = i.toString(); // i.toString converts an integer to a string, toString converts something to the string part
+gs.info(typeof i); // what type of?
 gs.info(typeof iStr);
 
-var n = parseInt(iStr); // parseInt converts string to a number 
+var n = parseInt(iStr); // parseInt converts string to an integer
 gs.info(typeof n + ' n=' + n);

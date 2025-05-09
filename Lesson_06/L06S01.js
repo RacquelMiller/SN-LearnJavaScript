ef762d7718625b55995ec3b2e69c29db8c8544eb
Racquel <racquelsteph@gmail.com>
2025-05-08 22:06:51 -0400

    Update L06S01.js

--- Lesson_06/L06S01.js
+++ Lesson_06/L06S01.js
@@ -3,7 +3,7 @@
 //
 var firstName = "Racquel";
 var lastName  = 'Miller';  // Either is ok
-var myHair    = "Racquel's Hair";  // Mix double quotation marks to avoid issues when using a single quotation mark as an apostrophe
+var myHair    = "Racquel's Beautiful Hair";  // Mix double quotation marks to avoid issues when using a single quotation mark as an apostrophe
 // Note the use of + when strings are involved
 var name = firstName + ' ' + lastName;
 gs.info(name);
